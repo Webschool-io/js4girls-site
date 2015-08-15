e0bc3a33f9f8b10a2cbc879df6e449adff0cd44b
Miika <mfabianad@gmail.com>
2015-08-15 01:49:29 -0300

    update site

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,20 @@
 // put your code here
 // remember, you can use modules with browserify
-console.log('Hey girls');
+
+$(function(){
+	menuCollapse = function() {
+		var openMenu = $("[data-attr='toglleMenu']"),
+			menu = $(".menu[data-attr='menu']"),
+			itemMenu = menu.find('a');
+
+		openMenu.on('click touchend', function(){
+			menu.toggleClass('is-closed');
+			console.log("menu");
+		});
+		itemMenu.on('click touchend', function(){
+			menu.addClass('is-closed');
+		});
+	}
+
+	menuCollapse();
+});
