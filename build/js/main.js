bcf154a547aad43cf6d78d06f117e3e480363450
Miika <mfabianad@gmail.com>
2015-08-16 13:30:54 -0300

    fix menu mobile

--- build/js/main.js
+++ build/js/main.js
@@ -2,19 +2,15 @@
 // remember, you can use modules with browserify
 
 $(function(){
-	menuCollapse = function() {
-		var openMenu = $("[data-attr='toglleMenu']"),
-			menu = $(".menu[data-attr='menu']"),
-			itemMenu = menu.find('a');
+	var toggleMenu = $("[data-attr='toggleMenu']");
 
-		openMenu.on('click touchend', function(){
-			menu.toggleClass('is-closed');
-			console.log("menu");
-		});
-		itemMenu.on('click touchend', function(){
-			menu.addClass('is-closed');
-		});
+	toggleMenu.on('click', function(){
+		menuCollapse();
+	});
+
+	menuCollapse = function() {
+		var menu = $("[data-attr='menu']");
+		menu.toggleClass('is-closed');
 	}
 
-	menuCollapse();
 });
